d03789d5a35dab7ad3f4000d1ea7c897f1806280
Gareth Williams <gareth@garethw.net>
2013-06-30 21:32:44 +0100

    Mono Windows build sorted

--- Hunted.Mono.Windows/Properties/AssemblyInfo.cs
+++ Hunted.Mono.Windows/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Other Space")]
+[assembly: AssemblyTitle("Hunted")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
